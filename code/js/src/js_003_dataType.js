8daf6dec81c3f998e8660b092cee11e10bd55d1e
xidoWeb <erivery2@gmail.com>
2021-06-14 18:22:27 +0900

    객체,배열,함수

--- code/js/src/js_003_dataType.js
+++ code/js/src/js_003_dataType.js
@@ -1,5 +1,5 @@
 // ../js/src/js_003_dataType.js
-
+// var  n, t, bool, nn, type, my;
 
 // 이름()  -> 함수 호출
 // 이름(매개변수) ->  함수를 호출할 때에는 매개체가 되는 변수를 넣는다.
@@ -26,15 +26,63 @@ var nn = null;
 type = typeof(nn);
 console.log(nn, type);
 
-console.log('my: ', my);
-
-
 var und = undefined;
 type = typeof(und);
 console.log(und, type);
 
-
-
-
-
+console.log('my: ', my);
 var my;
+// hoisting 현상 : 
+// 변수(함수)를 먼저 생성하지 않더라도, 상단으로 끌어올려지는 현상
+
+// var -> let, const
+
+var line = '------------------------';
+console.log( line );
+
+// 무엇에대한 정의를 판단하는 것 -> 객체 : object -> {}
+// 순서를 가질 수 없다.
+
+var obj = { 
+            'samsung':'galaxy', 
+            'apple' : 'iphone',
+            'google': 'pixel',
+            'ms' : 'surface',  
+            'other brand' : 'smart phone'
+          };
+console.log( obj['other brand'] );
+console.log( obj.samsung );
+console.log( obj.ms );
+
+type = typeof(obj);
+console.log(obj, type);
+
+console.log( line );
+// 목록의 나열 -> 배열 : array -> []
+// css는 첫번째를 1,  js는 첫번째를 0으로 판단
+// 순서를 가진다.
+var ary = [
+            'samsung', 'ms', 'google', 'lg', 'xiaomi'
+          ];
+console.log(ary);
+console.log(ary[2]);
+type = typeof(ary);
+console.log(type);
+
+var con;
+con = obj.constructor === Object;
+console.log('객체: {}', con);
+
+con = ary.constructor === Array;
+console.log('배열: []', con);
+
+// = 대입, == 값만비교, === 값/형타입 모두 비교
+
+console.log( line );
+
+function myFn(n){
+  var result = n +'님 어서오세요'; 
+  return result;
+}
+
+console.log( myFn('xido') );
