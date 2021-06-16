f92f7d76a09abe8435589449b998d8b3119a2d73
xidoWeb <erivery2@gmail.com>
2021-06-16 16:23:01 +0900

    배열 추가/삭제

--- code/js/src/js_010_object_method_01.js
+++ code/js/src/js_010_object_method_01.js
@@ -1,2 +1,27 @@
 // js/src/js_010_object_method_01.js
 
+var drink;
+// drink = new Object;
+// drink = new Array;
+    drink = [];
+    drink[0] = 'orange';
+    drink[1] = 'grape';
+    
+    // push(뒤에추가), pop():뒤빼기 unshift(앞추가),  shift(): 앞 빼기
+    drink.push('banana');
+    drink.push('kiwi');
+    drink.push('avocado');
+    drink.push('lemon');
+
+    drink.pop();
+    drink.pop();
+    drink.pop();
+
+    drink.shift();
+    drink.shift();
+
+    drink.unshift('americano');
+    drink.unshift('milk');
+
+console.log( drink );
+
