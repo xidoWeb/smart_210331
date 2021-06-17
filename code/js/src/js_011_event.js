d97eec9db5c2b7b50311f922788564b79b63daf2
xidoWeb <erivery2@gmail.com>
2021-06-17 15:22:58 +0900

    js - event 기능 처리 전 세팅

---
+++ code/js/src/js_011_event.js
@@ -0,0 +1,7 @@
+// js_011_event.js
+
+var evtUl = document.getElementsByClassName('event_list')[0];
+var evtLi = evtUl.getElementsByTagName('li');
+// var evtLi = evtUl.children('li');
+
+evtLi[0].style.border = '2px solid #f00';
