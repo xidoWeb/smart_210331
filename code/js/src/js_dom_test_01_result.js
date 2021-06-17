612987d68fc045101d93c4658b2bb8e14799107b
xidoWeb <erivery2@gmail.com>
2021-06-17 13:14:56 +0900

    js - 객체, 생성자함수를 이용하여 리스트처리

--- code/js/src/js_dom_test_01_result.js
+++ code/js/src/js_dom_test_01_result.js
@@ -20,13 +20,20 @@ var product = procuctArea.getElementsByClassName('product')[0];
 //   '카드에들어갈 이미지 설명1',
 //   '카드 이미지'
 // ];
-
+// -------------------------------------------------------------------
+// sample data 형식
 var dataCheck = {
   link   : 'http://naver.com',
   imgSrc : '../img/undraw/undraw_001.png',
   imgNarr : '카드에들어갈 이미지 설명_001',
   cardTitle : '카드 제목 _ 001'
 };
+// ========================================================================
+// 틀 제작 ----------------------------------------------------------------
+// makeLiFn 함수로 카드를 반복으로 생성하기 위해 해당하는 내용을 담는 틀 제작
+// 내용을 담을 내용들이 여러개로 복합적으로 담기기 때문에 listData로 매개변수 제작
+// dataCheck변수는 차후에 makeLiFn으로 호출하였을때 listData로서 인수/인자로 변경
+// 차후 dataCheck변수 대신 다른 내용이 들어갈 수 있도록 설정 -> dataCheck는 임시로 만든 형식
 
 var makeLiFn = function (listData){
   var makeLi = document.createElement('li');
@@ -47,8 +54,62 @@ var makeLiFn = function (listData){
   // ---------------------------
   product.appendChild(makeLi);
 }
+// ---------------------------------------------------
+// data 형식 생성
+var imgUrl = '../img/undraw/';
 
+var cardDataAll = [
+  {
+    link   : 'http://naver.com',
+    imgSrc : 'undraw_001.png',
+    imgNarr : '카드에들어갈 이미지 설명_001',
+    cardTitle : '카드 제목 _ 001'
+  },
+  {
+    link   : 'http://daum.net',
+    imgSrc : 'undraw_002.png',
+    imgNarr : '카드에들어갈 이미지 설명_002',
+    cardTitle : '카드 제목 _ 002'
+  },
+  {
+    link   : 'http://daum.net',
+    imgSrc : 'undraw_003.png',
+    imgNarr : '카드에들어갈 이미지 설명_003',
+    cardTitle : '카드 제목 _ 003',
+    cardPar : 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    link   : 'http://daum.net',
+    imgSrc : 'undraw_004.png',
+    imgNarr : '카드에들어갈 이미지 설명_004',
+    cardTitle : '카드 제목 _ 004',
+    cardPar : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ratione inventore excepturi exercitationem illo ullam nihil hic sit fugit veritatis?'
+  },
+];
+
+// console.log( imgUrl + cardDataAll[1].imgSrc );
+
+// 실제로 사용하기 위해 위 복합으로 만들어진 data를 개별로 분리해서 쓸수 있게
+// 불필요한 data는 불러오지 않게 처리
+// 생성자함수를 제작
+
+function MakeCard(url, data){
+  this.link  = data.link;
+  this.imgSrc = url + data.imgSrc;
+  this.imgNarr = data.imgNarr;
+  this.cardTitle = data.cardTitle;
+}
+
+// var setCard = new MakeCard( imgUrl, cardDataAll[0] );
+// console.log( setCard.imgSrc );
+
+// --------------------------------------------------
+// 최종 동작
 var i = 0;
-for( ; i < 10; i += 1 ){
-  makeLiFn(dataCheck);
+var setCard;
+var cardLen = cardDataAll.length;
+
+for( ; i < cardLen; i += 1 ){
+  setCard = new MakeCard( imgUrl, cardDataAll[i] );
+  makeLiFn(setCard);
 }
