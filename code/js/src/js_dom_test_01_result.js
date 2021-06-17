8d8ae48c8c9555c9aac467aa27733a7a0eeabdf1
xidoWeb <erivery2@gmail.com>
2021-06-17 10:24:27 +0900

    js - 리스트형식의 카드메뉴 만들기 -틀잡기

--- code/js/src/js_dom_test_01_result.js
+++ code/js/src/js_dom_test_01_result.js
@@ -1,1 +1,24 @@
 // js/src/js_dom_test_01_result.js
+
+// var procuctArea = document.querySelector('.product_area');
+//.product_area .product {}
+var procuctArea = document.getElementsByClassName('product_area')[0];
+var product = procuctArea.getElementsByClassName('product')[0];
+
+
+// 1차 목표 : li요소를 생성하여, product내부에 삽입 (여러개)
+// 생성 - createElement()
+// 삽입 - appendChild()
+
+var makeLiFn = function (){
+  var makeLi = document.createElement('li');
+  product.appendChild(makeLi);
+}
+
+makeLiFn();
+makeLiFn();
+makeLiFn();
+makeLiFn();
+makeLiFn();
+makeLiFn();
+makeLiFn();
