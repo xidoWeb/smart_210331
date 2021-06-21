1e53950af7ada880f512892b75c3d83d6d9f71ea
xidoWeb <erivery2@gmail.com>
2021-06-21 14:39:46 +0900

    js-jQuery 최대한 비슷하게 기능처리

--- code/js/src/jq_001_import_jQuery.js
+++ code/js/src/jq_001_import_jQuery.js
@@ -2,40 +2,41 @@
 
 (function($){
 
-// javascript 
-/*
+/* // javascript 
   var h1 = document.getElementsByTagName('h1')[0];
   var h1Link = h1.getElementsByTagName('a')[0];
-  h1Link.style.fontSize = '16px';
-  h1Link.style.color = "#07f";
   var p = document.getElementsByTagName('p')[0];
+  var btn = document.getElementsByTagName('button')[0];
+
+  h1Link.style = 'font-size:16px; color:#07f';
   p.style.display = 'none';
 
-  var btn = document.getElementsByTagName('button')[0];
   btn.addEventListener('click', function(e){
     e.preventDefault();
-    p.style.display = 'block';
-    p.style.opacity = 0;
-    p.style.transition = 'all 1000ms ease';
+    p.style = 'display:block; opacity:0; transition:all 1000ms ease';
 
-    setTimeout(function(){
-      p.style.opacity = 1;
+    setTimeout(function(){  
+      p.style.opacity = 1;  
     },10);
-
   });
 */
   
 
-/* jQuery */
+/* //jQuery */
+
   var h1 = $('h1');
   var h1Link = h1.children('a');
   var p = $('p');
+  var btn = $('button');
+  
   p.hide();
   h1Link.css({ 'fontSize': '16px', 'color': '#07f' });
 
-  var btn = $('button');
   btn.on('click', function(e){
     e.preventDefault();
-    p.fadeIn(600);
+    p.fadeIn(1000);
   });
+
+
+
 })(jQuery);
