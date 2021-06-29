8fd56a844ec6f8ce9d00add8e5af126830f72bb5
xidoWeb <erivery2@gmail.com>
2021-06-29 13:13:45 +0900

    jQuery - text 변경방법

---
+++ code/js/src/js_013_gallery_01.js
@@ -0,0 +1,33 @@
+// js_013_gallery_01.js
+(function($){
+// jQuery
+
+
+// 1. li클릭시(사실은 내부의 a를 클릭)
+// 2. 내부의 a에 들어있는 data-text속성값을 
+// 3. .big_image내부p에 전달
+
+
+var gallery_01 = $('.gallery_01');
+var galUl = gallery_01.children('ul');
+var galLi = galUl.children('li');
+
+var gal_01_Box = $('.gallery_box_01');
+var galP = gal_01_Box.children('p');
+
+var liConvertText = function(event){
+  event.preventDefault();
+
+  var thisLink = $(this).children('a');
+  var thisData = thisLink.attr('data-text');
+  // console.log( thisData );
+  galP.text(thisData);
+
+}
+
+galLi.on('click', function(event){
+  liConvertText();
+});
+
+
+})(jQuery);
