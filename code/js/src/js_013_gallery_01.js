bdfdda62aaf73ea67fe0e58fbc0efefef05b7a0f
xidoWeb <erivery2@gmail.com>
2021-06-29 15:51:29 +0900

    jquery - gallery_image적용_001

--- code/js/src/js_013_gallery_01.js
+++ code/js/src/js_013_gallery_01.js
@@ -28,10 +28,24 @@ var liConvertText = function(){
 galLi.on('click', liConvertText() );
 // ---------------------------------------------
 
-// 1. li클릭시(사실은 내부의 a를 클릭)
-// 2. 내부의 a에 들어있는 data-text속성값을 
-// 3. .big_image내부p에 전달
+// 1. .gallery_02 li클릭시(사실은 내부의 a를 클릭)
+// 2. 내부의 a에 들어있는 data-image속성값을 
+// 3. .big_image내부에 전달
 
+var gal_02 = $('.gallery_02');
+var gal_02_box = gal_02.find('.big_image');
+var gal_02_list = gal_02.find('li');
+var galUrl = '../img/gallery/u_big/';
+
+gal_02_list.children('a').on('focus click', function(e){
+  e.preventDefault();
+  // 해당요소의 속성 (data-image) 값을 찾아라
+  var select = $(this);
+  var selectImg = select.attr('data-image');
+  // console.log( selectImg );
+  var useImg = galUrl + selectImg;
+  gal_02_box.css({'backgroundImage':'url(' + useImg + ')'});
+});
 
 
 
