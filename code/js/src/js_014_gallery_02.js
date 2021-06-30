c388545413acf6d213e839d36c5741c1f25fb6ad
xidoWeb <erivery2@gmail.com>
2021-06-30 12:57:54 +0900

    jquery - gallery 객체내용 list에 적용

--- code/js/src/js_014_gallery_02.js
+++ code/js/src/js_014_gallery_02.js
@@ -2,10 +2,51 @@
 (function($){
 // jQuery
 
+
+var galleryData = [
+  {
+    miniImg:'agenlaku.jpg',
+    miniText:'thumnail_01',
+    bigImg:'agenlaku-indonesia-FoUETBpb6mY-unsplash.jpg',
+    bigText:'001 - 상세 설명 첨부'
+  },
+  {
+    miniImg:'alexander.jpg',
+    miniText:'thumnail_02',
+    bigImg:'alexander-andrews-BX4Q0gojWAs-unsplash.jpg',
+    bigText:'002 - 상세 설명 첨부'
+  },
+  {
+    miniImg:'annie.jpg',
+    miniText:'thumnail_03',
+    bigImg:'annie-spratt-cxAZxTuL7Sk-unsplash.jpg',
+    bigText:'003 - 상세 설명 첨부'
+  },
+  {
+    miniImg:'bench.jpg',
+    miniText:'thumnail_04',
+    bigImg:'bench-accounting-nvzvOPQW0gc-unsplash.jpg',
+    bigText:'004 - 상세 설명 첨부'
+  },  
+  {
+    miniImg:'c.jpg',
+    miniText:'thumnail_05',
+    bigImg:'c-d-x-PDX_a_82obo-unsplash.jpg',
+    bigText:'005 - 상세 설명 첨부'
+  }
+];
+
+
 var makeLi = '<li><a href="#"><span>이미지_01</span></a></li>';
 var gallery = $('.gallery');
 var thumnail = gallery.find('.thumnail');
 var big_image = gallery.find('.big_image');
+
+var thumUrl = '../img/gallery_02/mini/';
+var bigUrl = '../img/gallery_02/origin/';
+
+var thumnailLi, listSource, listText;
+
 
 // [v] 1. 반복문을 이용하여 big_image에 li를 생성
 // 2. 이미지를 담는 객체를 생성하여 담기
@@ -13,10 +54,20 @@ var big_image = gallery.find('.big_image');
 
 // for(최초;조건;증감){}
 var i=0;
-for( ; i < 5 ; i += 1 ){
+var thumLength = galleryData.length;
+for( ; i < thumLength ; i += 1 ){
   // thumnail내부에 makeLi를 채우기(append : 내부에 뒤에 채우기)
   thumnail.append(makeLi);
+  thumnailLi = thumnail.children('li').eq(i);
+  listSource = 'url(' + thumUrl + galleryData[i].miniImg + ')';
+  listText = galleryData[i].miniText;
+
+  thumnailLi.children('a').css({backgroundImage:listSource});
+  thumnailLi.find('span').text(listText);
 }
+
+// 전체 thumnail 내부의 li를 재선정
+thumnailLi = thumnail.children('li');
 
 
 
