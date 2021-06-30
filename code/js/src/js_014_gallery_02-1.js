bf18c32a68fec1d96fd4b8a44167bd958850d6e3
xidoWeb <erivery2@gmail.com>
2021-06-30 17:46:32 +0900

    jquery - 임시 세팅

---
+++ code/js/src/js_014_gallery_02-1.js
@@ -0,0 +1,32 @@
+// js_014_gallery_02-1.js
+(function($){
+// jQuery
+
+var modal = $('.modal');
+var mldalLi = modal.find('li');
+
+var modalView = $('.modal_view');
+var closeBtn = $('.close_btn');
+
+modalView.css({width:'700px', height:'500px', border:'1px solid #333'});
+closeBtn.css({width:'60px', height:'60px', background:'#d7a'});
+modalView.hide();
+
+mldalLi.on('click', function(e){
+  e.preventDefault();
+  var getColor = $(this).children('a').attr('data-color');
+
+  modalView.css({'backgroundColor':getColor});
+  modalView.show();
+
+});
+
+closeBtn.on('click', function(e){
+  e.preventDefault();
+  modalView.hide();
+});
+
+// 순서는 어떻게 진행해야 하는지
+
+
+})(jQuery);
