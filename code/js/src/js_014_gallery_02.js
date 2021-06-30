a527679c024918f38457c92cfb4d73dccf7a6dd9
xidoWeb <erivery2@gmail.com>
2021-06-30 15:19:25 +0900

    jquery -  gallery 두번째 fade기능을 처리하기 위해 세팅

--- code/js/src/js_014_gallery_02.js
+++ code/js/src/js_014_gallery_02.js
@@ -36,42 +36,11 @@ var galleryData = [
   }
 ];
 
-
-var makeLi = '<li><a href="#"><span>이미지_01</span></a></li>';
-var gallery = $('.gallery');
-var thumnail = gallery.find('.thumnail');
-var big_image = gallery.find('.big_image');
-
-var thumUrl = '../img/gallery_02/mini/';
-var bigUrl = '../img/gallery_02/original/';
-var thumnailLi, listSource, listText;
-
+// ------------------------------------------------
 // [v] 1. 반복문을 이용하여 big_image에 li를 생성
 // [v] 2. 이미지를 담는 객체를 생성하여 담기
 // [v] 3. 큰 영역에 첫번째 이미지를 담기
-
-// for(최초;조건;증감){}
-var i=0;
-
-big_image.css({backgroundImage:'url('+ bigUrl + galleryData[i].bigImg +')'});
-
-var baseLiSetFn = function(i){
-   // thumnail내부에 makeLi를 채우기(append : 내부에 뒤에 채우기)
-   thumnail.append(makeLi);
-   thumnailLi = thumnail.children('li').eq(i);
-   listSource = 'url(' + thumUrl + galleryData[i].miniImg + ')';
-   listText = galleryData[i].miniText;
- 
-   thumnailLi.children('a').css({backgroundImage:listSource});
-   thumnailLi.find('span').text(listText);
-};
-
-
-var thumLength = galleryData.length;
-for( ; i < thumLength ; i += 1 ){
-  baseLiSetFn(i);
-}
-// ------------------------------------------------------
+// ------------------------------------------------
 // [v] 1. 작은 이미지를 마우스 올릴경우 다음과 같은 기능을 수행
 // [v] 2. 작은이미지에 들어있는 이미지와 같은형태의 큰이미지의 주소를 찾아서
          // bigUrl + galleryData[i].gigImg
@@ -95,6 +64,38 @@ for( ; i < thumLength ; i += 1 ){
 // 응용방법: 배너광고형태 중 fade형식으로 되는 형태를 이해 할 수 있다.
 //-----------------------------------------------------
 
+var makeLi = '<li><a href="#"><span>이미지_01</span></a></li>';
+var gallery = $('.gallery');
+var thumnail = gallery.find('.thumnail');
+var big_image = gallery.find('.big_image');
+
+var thumUrl = '../img/gallery_02/mini/';
+var bigUrl = '../img/gallery_02/original/';
+var thumnailLi, listSource, listText;
+
+// for(최초;조건;증감){}
+var i=0;
+
+big_image.css({backgroundImage:'url('+ bigUrl + galleryData[i].bigImg +')'});
+
+var baseLiSetFn = function(i){
+   // thumnail내부에 makeLi를 채우기(append : 내부에 뒤에 채우기)
+   thumnail.append(makeLi);
+   thumnailLi = thumnail.children('li').eq(i);
+   listSource = 'url(' + thumUrl + galleryData[i].miniImg + ')';
+   listText = galleryData[i].miniText;
+ 
+   thumnailLi.children('a').css({backgroundImage:listSource});
+   thumnailLi.find('span').text(listText);
+};
+
+var thumLength = galleryData.length;
+for( ; i < thumLength ; i += 1 ){
+  baseLiSetFn(i);
+}
+// ------------------------------------------------------
+
+
 // 중복기능 함수로 제작
 var setGallerySystemFn = function( idx ){
   var bigImgSet = bigUrl + galleryData[ idx ].bigImg;
@@ -106,6 +107,8 @@ var setGallerySystemFn = function( idx ){
 // ---------------------------------------------------
 // 전체 thumnail 내부의 li를 재선정
 thumnailLi = thumnail.children('li');
+
+setGallerySystemFn(2);
 
 thumnailLi.on('mouseenter', function(e){
   var _thisI = $(this).index();
