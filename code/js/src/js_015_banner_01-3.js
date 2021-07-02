c983940dcece6a706b70c78fedc7433312beeaa3
xidoWeb <erivery2@gmail.com>
2021-07-02 17:06:09 +0900

    slide_03 코드 다음버튼수행 -정리

--- code/js/src/js_015_banner_01-3.js
+++ code/js/src/js_015_banner_01-3.js
@@ -21,7 +21,6 @@ var productLi = productUl.children('li');
 // 임의 변수/변수초기화 =============================
 var count = 0;
 
-
 // 함수영역 =============================
   //현재 배너위치 체크
 var nowCFn = function(){
@@ -43,13 +42,10 @@ var mvFn = function(){
 
   var cloneLi = productLi.eq(-1).clone();
   productUl.prepend(cloneLi);
-   
-
 
   // li갯수 파악 및, total에 삽입
   var liLen = productLi.length;
   countTotal.text(liLen);
-
   // 현재 배너 위치 체크
   nowCFn();
 
@@ -58,60 +54,29 @@ var newProductLi = productUl.children('li');
 var newLiLen = newProductLi.length;
 console.log(newLiLen);
 
-// 1. 범위 파악
-// var productWidth = productArea.width();
+// 1. 범위 파악(%로 대처)
 // 2. 전체 넓이 파악후 재배치(변경된 li개수를 파악후 처리)
-// productUl.css({'width': (productWidth * newLiLen) + 'px' });
 productUl.css({'width': (100 * newLiLen) + '%' });
 // 3. 각각의 넓이 재배치(변경된 li요소들의 갯수와 크기수정)
 newProductLi.css({'width': (100 / newLiLen) + '%'});
 // 4. 처음 보이는 내용을 첫li요소로 변경
 productUl.css({'position':'relative', 'left': -100+'%'});
 
-
 // 이벤트 ===================================
 var permission = true;
-
 slideBtn.on('click', function(e){
   e.preventDefault();
-
   if(permission){
     permission = false;
-
     // 다음 버튼클릭했으니 이제 이동
     count += 1; 
-    // var mv = -(100 * count) +'%';
-
-    /*
-      productUl.animate({marginLeft: mv},function(){
-        // 애니메이션 기능 처리 후 수행
-        if(count >= liLen) { count = 0; }
-        mv = -(100 * count) +'%';
-        productUl.css({marginLeft: mv});
-        console.log( count );
-        nowCFn();
-      });
-    */
-
     if(count >= liLen){
       productUl.css({marginLeft: 100+ '%'});
       count = 0;
-      // productUl.animate({marginLeft: 0});
     }
-    // else{
-      // productUl.animate({marginLeft: mv});
-    // }
-
-    productUl.stop().animate({marginLeft: mvFn()}, function(){
-      permission = true;
-    });
-
+    productUl.stop().animate({marginLeft: mvFn()}, function(){ permission = true; });
     nowCFn();
-
   } // permission 조건
-
 });// slideBtn.on('click' ...)
-
-
 
 })(jQuery);
