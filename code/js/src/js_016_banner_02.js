a0ea9c3fe136eac60a50e8cfe7bcea41a9592dfd
xidoWeb <erivery2@gmail.com>
2021-07-05 18:24:53 +0900

    slidebanner - 외부이미지 불러오기/적용

--- code/js/src/js_016_banner_02.js
+++ code/js/src/js_016_banner_02.js
@@ -2,10 +2,18 @@
 
 (function($){
 // jQuery
+// data 작성
+
+// $.ajax({}).done(function(data){});
+$.ajax({
+  url:"../data/banner_02.json",
+  context: document.body
+}).done(function(data){
+    var baseUrl = "../img/gallery/";
+    var viewData = data;
 
   // 1. 다음버튼 클릭시 슬라이드 넘어가기
   // 2. 이전버튼 클릭시 슬라이드 이전내용 넘어가기
-  
   
   // 변수
   var slideBtn = $('.slide_btn');
@@ -20,7 +28,29 @@
   var n = 0;
   var permission = true;
 
-  
+  // -----------------------------------
+  // 1000. li요소에 각각 내용 삽입
+
+  var viewInnerTextFn = function(i){
+    var li        = viewLi.eq(i);
+    var viewDiv   = li.children('div');
+    var viewTitle = viewDiv.children('h4');
+    var viewCon   = viewDiv.children('p');
+    var viewLink  = viewDiv.find('a');
+    
+    li.css({'backgroundImage':'url('+ baseUrl + viewData[i].bgImg +')'});
+    viewTitle.text( viewData[i].title );
+    viewCon.text( viewData[i].content );
+    viewLink.attr( 'href', viewData[i].link );
+  };
+
+  var i = 0; 
+  for(; i< viewLiLen; i+=1){
+    viewInnerTextFn(i);
+  }
+
+  // --------------------------------------
+
   // 100. 1칸씩 이동
   // 200. 이동 제한(최대값)
   // 300. 무한으로 돌아가게 만들기
@@ -69,5 +99,18 @@
     } // if(permission){
   }); //  prevBtn.on('click' ...)
   
-
+  }); // $.ajax().done()
 })(jQuery);
+
+
+/*
+(function($){
+  $.ajax({
+    url:'불러올 파일',
+    context:document.body
+  }).done(function(data){
+    var data = data;
+    // jQuery
+  });
+})(jQuery);
+*/
