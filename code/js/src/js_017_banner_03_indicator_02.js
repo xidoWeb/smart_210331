9173bbec9e57ec7a88430a718a933d386f85fd55
xidoWeb <erivery2@gmail.com>
2021-07-06 14:48:26 +0900

    setIntervel 임시 정지

--- code/js/src/js_017_banner_03_indicator_02.js
+++ code/js/src/js_017_banner_03_indicator_02.js
@@ -6,8 +6,11 @@
   // 102. [v] 클릭한 인디케이터의 위치에따른 광고의 위치?
   // 103. [v] 선택한 순서에 맞는 인디케이터에 act를 담기
   // ------------------------------------------------
-  // 104. 일정시간마다 광고배너영역이 움직이도록 처리
-
+  // 104. [v] 일정시간마다 광고배너영역이 움직이도록 처리 -> setInterval
+  // 105. 마우스 올리면 정지 -> clearInterval
+  // 106. 일정시간마다 움직이다 마우스올리면 정지, 벗어나면 다시 동작
+  // 107. 마지막 내용이 보일때 처음으로 이동하는 형태를 수정
+  // ------------------------------------------------
 
   // 변수
   var banner = $('.banner_02');
@@ -39,14 +42,25 @@
     slideMoveFn(n);
   });
 
-  // 반복기능
-  setInterval(function(){
+  // 반복기능 -> clearInterval에서 제어하기위해 변수로 지정
+  // 변수로 지정하더라도, 함수자체는 동작을 한다.
+ var autoMoveFn = setInterval(function(){
     // n의 수치가 일정범위 내에서 처리되는것을 파악
     n += 1;
     if(n >= indiLiLen){ n = 0; }
     // console.log( n );
     slideMoveFn(n);
   }, timed*4);
+
+  // 마우스 올리면 setInterval을 삭제
+  banner.on('mouseenter', function(){
+    clearInterval( autoMoveFn );
+  });
+
+  // setInverval 함수를 넣었다, 뺏다 가능하도록 제작 -> 함수처리
+
+
+
 
   // setInterval(기능수행, 시간); -> 일정 시간 마다 기능을 수행
   // setTimeout(기능수행, 시간); ->  일정 시간 후에 기능을 수행
