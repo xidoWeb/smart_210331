2b6c8b5d3cd14ad53cc22bc9bfdb2b5006237fa6
xidoWeb <erivery2@gmail.com>
2021-07-09 13:11:20 +0900

    터치 및 마우스를 움직일경우 좌표 체크

--- code/js/src/jq_006_mouseck_xy.js
+++ code/js/src/jq_006_mouseck_xy.js
@@ -2,10 +2,6 @@
 (function($){
 // jQuery
 
-  var area = $('.area');
-  var chType = $('.check_type');
-  var chX    = $('.check_x');
-  var chY    = $('.check_y');
 
   // click은 마우스눌렀다 띄는 행위전체, 왼버튼으로 인식되어있으며 button값은 0
 
@@ -21,15 +17,40 @@
   // touchend 터치가능한 기기를띌때
   // touchmove 터치중 손가락을 움직일때
 
+  // client : 브라우저에서 보이는부분만큼을 왼쪽을 0, 상단을0로 무조건본다.
+  // offset : 이벤트 발생영역 -> 터치기반에는 offsetX, offsetY 존재하지 않는다.
+  // page : 문서전체의 크기를 기준으로 본다.
+  // screen : 모니터 좌표기준
+
+// ---------------------------------------------------
+  var area = $('.area');
+  // var area = $('#wrap');
+  var chType = $('.check_type');
+  var chX    = $('.check_x');
+  var chY    = $('.check_y');
+
   // 체크 및 좌표함수
-  var checkTypeFn = function(e){
+  var checkTypeFn = function(e){    
     var eType = e.type;
-    chType.text(eType);
+    var evt = e.originalEvent;
+
     var x = 0;
-    var y = 0;
+    var y = 0;   
+    chType.text(eType);
+
+    if(e.type === 'touchmove'){
+      x = evt.changedTouches[0].offsetX ;
+      y = evt.changedTouches[0].offsetY ;
+      console.log( evt.changedTouches[0] );
+    }else if(e.type === 'mousemove'){
+      x = evt.offsetX;
+      y = evt.offsetY;
+    }
+    
     chX.text(x);
     chY.text(y);
   };
+
 
 
   // mousemove
@@ -41,44 +62,44 @@
   area.on('mousedown', function(e){
     if(e.button === 0) {
       option = true;
-      console.log('마우스 왼버튼 클릭으로 move기능 승인');
+      // console.log('마우스 왼버튼 클릭으로 move기능 승인');
     }else{
-      console.log('마우스 왼버튼만 승인가능합니다.');
+      // console.log('마우스 왼버튼만 승인가능합니다.');
     }
   });
 
   area.on('mouseup', function(e){
     option = false;
-    console.log('마우스 move기능 해제');
+    // console.log('마우스 move기능 해제');
     chType.text('none');
-    setTimeout(function(){
-      console.clear();
-    }, 3000);
+    // setTimeout(function(){
+    //   console.clear();
+    // }, 3000);
   });
 
   area.on('mousemove', function(e){
     if(option){
       checkTypeFn(e)
-      console.log('마우스 움직이는 중');
+      // console.log('마우스 움직이는 중');
     }
   });
 
   area.on('touchstart', function(e){
     checkTypeFn(e);
-    console.log('이곳이 터치의 시작점!!!');
+    // console.log('이곳이 터치의 시작점!!!');
   });
 
   area.on('touchend', function(e){
     checkTypeFn(e);
-    console.log('이곳이 터치의 끝점!!!');
-    setTimeout(function(){
-      console.clear();
-    }, 3000);
+    // console.log('이곳이 터치의 끝점!!!');
+    // setTimeout(function(){
+    //   console.clear();
+    // }, 3000);
   });
 
   area.on('touchmove', function(e){
     checkTypeFn(e);
-    console.log('터치로 움직이는 중');
+    // console.log('터치로 움직이는 중');
   });
 
 
