aa7a93fe9168155fe3d2026102a7b26c5142b211
xidoWeb <erivery2@gmail.com>
2021-07-09 10:34:46 +0900

    전체 intro 코드내용 수정

--- code/js/jquery/all_contents.js
+++ code/js/jquery/all_contents.js
@@ -1,6 +1,7 @@
 // all_contents.js
 (function($){
 // jQuery
+  var part = $('.part');
   var h2 = $('h2');
   var h2Con;
   var nth = function(n){
@@ -17,6 +18,22 @@
     h2.eq(i).find('button').text( k );
   }
 
+  h2.children('button').on('click', function(e){
+    e.preventDefault();
+    var i = $(this).parents('.part').index() - 2;
+    // console.log( i );
+    part.eq(i).find('ol').stop().slideToggle(function(){
+      var ck =  $(this).css('display') === 'none';
+      if(ck){
+        part.eq(i).removeClass('act');
+      }else{
+        part.eq(i).addClass('act');
+      }
+    });
+    part.eq(i).siblings().find('ol').stop().slideUp(function(){
+      part.removeClass('act');
+    });
 
+  });
 
 })(jQuery);
