a061505e40ab9fb2023d8c12ebb33e387cd4fd31
xidoWeb <erivery2@gmail.com>
2021-07-12 13:07:33 +0900

    scroll처리시 수행내용 세팅

---
+++ code/js/src/js_021_scroll.js
@@ -0,0 +1,12 @@
+// js_021_scroll.js
+(function($){
+// jQuery
+
+// 1. #headBox의 위치가 상단에서 얼만큼 떨어졌는지 체크 -> offset().top
+// 2. 스크롤시 스크롤값을 체크 -> scrollTop()
+// 3. 스크롤의 값이 offset().top의 값보다 커지면 headBox의 위치를 position:fixed
+// 4. offset().top의 값은 수시로 확인할 필요가 없다.
+
+
+
+})(jQuery);
