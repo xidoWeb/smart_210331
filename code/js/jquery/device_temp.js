afba4b760787761adf6dbf10c65534d156681ef9
xidoWeb <erivery2@gmail.com>
2021-07-13 14:23:27 +0900

    접속브라우저를 확인하기 위해 명확한 내용 파악을 위한 구성으로 재편성

--- code/js/jquery/device_temp.js
+++ code/js/jquery/device_temp.js
@@ -3,36 +3,55 @@
 // jQuery
 // 1. 접속 브라우저 환경
 // 2. 브라우저의 크기값을 파악
-
   // 변수 
-  
+
   var win = $(window);
 
   // 2. 접속 브라우저 환경
   var browserSetFn = function(){
-    var browser = ['chrome', 'safari', 'opr', 'firefox'];
+
+    // var browser = ['opr', 'edg', 'chrome', 'safari', 'firefox'];
+    // 접속 브라우저를 파악하기 위해 표기된 용어와, 실제 확인용어 구분, 순서를 명확하게 파악
+    var browser = [
+      {type:'Opera', check:'opr'},
+      {type:'Edge', check:'edg'},
+      {type:'Chrome', check:'chrome'},
+      {type:'Safari', check:'safari'},
+      {type:'Fire fox', check:'firefox'}
+    ];
+
+    // 접속브라우저 체크(대소문자 구분없이 파악하기 위해 소문자로 설정)
     var checkDevice = navigator.userAgent.toLowerCase();
-    // console.log(checkDevice);
     var n = 0
     var ckIndex;
-    // '반응형웹이 불가능한 브라우저'; 
     var useRwd = false;
+    // IE브라우저 체크(ms엔진인 Trident엔진이 있을경우 IE브라우저) : navigator.userAgent.search('Trident') 
+    if(navigator.userAgent.search('Trident') !== -1){
+      // '반응형웹이 불가능한 브라우저'; 
+      useRwd = false;
+      console.log('사용브라우저:', 'ie');
+    }else{
+      // IE외에 다른브라우저 기반체크 (위 변수 browser변수의 순서를 잘 작성해야 해당브라우저를 파악이 가능)
+        for(; n < browser.length ; n++){
+          // indexOf() 존재유무 판단 -> -1 값을 나타나면 없다, 이외의 수치가 나오면 해당하는 위치에 있다
+          ckIndex = checkDevice.indexOf(browser[n].check);
+          // console.log(ckIndex);
+          if(ckIndex !== -1){
+            //'반응형 웹 가능한 브라우저';
+            useRwd = true;
+            console.log('사용브라우저:', browser[n].type);
+            // 체크된 상황에서 반복수행하지 않도록 처리
+            break; 
+          }
+        }// for
+    }// if:IE체크
 
-    for(; n < browser.length ; n++){
-      // indexOf() 존재유무 판단 -> -1 값을 나타나면 없다, 이외의 수치가 나오면 해당하는 위치에 있다
-      ckIndex = checkDevice.indexOf(browser[n]);
-      // console.log(ckIndex);
-      if(ckIndex !== -1){
-        //'반응형 웹 가능한 브라우저';
-        useRwd = true;
-        break;
-      }
-    }
-    console.log( useRwd );
+    // 반응형웹 '제작가능여부(true or false)'를 반환
     return useRwd;
   }// browserSetFn();
   var rwdCheck = browserSetFn();
   
+  // 반응형웹구현이 불가능할경우
   if(!rwdCheck){
     alert('접속 브라우저가 반응형구현 또는 flex 구조가 아니기 때문에 사용하기 불편할 수 있습니다.');
   } 
@@ -55,12 +74,12 @@
   };
 
   var beforeWidth = deviceWidth();
-  console.log('before: ', beforeWidth);
+  console.log('브라우저 변경 전 가로 크기 : ', beforeWidth);
 
   // 브라우저의 크기가 변경되면 해당 수치를 파악
   win.on('resize', function(){
     var afterWidth = deviceWidth();
-    console.log('after: ', afterWidth);
+    console.log('브라우저 변경 후 가로 크기 : ', afterWidth);
 
     // 디바이스 환경이 변경되면(같지 않으면)
     if(beforeWidth !== afterWidth){
