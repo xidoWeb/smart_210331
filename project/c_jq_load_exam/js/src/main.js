534e7f843603f30dad65d7c847e1ebdd8f40f753
xidoWeb <erivery2@gmail.com>
2021-07-16 12:24:04 +0900

    파일분리(main.html문서기준으로 체크)

---
+++ project/c_jq_load_exam/js/src/main.js
@@ -0,0 +1,18 @@
+// main.js
+(function($){
+// 현재 위치에서 주업무는 외부파일을 불러와서 정리/배치
+
+// 1. #headBox내부에 외부파일 header.html을 불러와서 처리
+// 2. .page_content내부에 외부파일 page_content.html불러오기
+// 3. #footBox내부에 외부파일 footer.html불러오기
+
+var headBox = $('#headBox');
+var pageCon = $('.page_content');
+var footBox = $('#footBox');
+
+headBox.load("./common/header.html");
+pageCon.load("./main/page_content.html");
+footBox.load("./common/footer.html");
+
+
+})(jQuery);
