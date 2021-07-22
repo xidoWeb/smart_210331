11d04d9012f71d325bf5d363cc638342d63632ef
xidoWeb <erivery2@gmail.com>
2021-07-22 15:12:09 +0900

    parallax_01 li요소 각각 처리

--- code/js/src/js_024_parallax_01.js
+++ code/js/src/js_024_parallax_01.js
@@ -8,7 +8,6 @@
 4. 화면의 하단(시작부분) 상단에있는 영역(끝부분)
 */
 
-
 // 변수
 var win = $(window);
 var winH = win.outerHeight();
@@ -24,58 +23,40 @@ var partLi = part.find('li');
 // 기준치가 li의 시장점이 아닌 중간지점으로 교체
 // 실제 이동하는 이미지의 background-position-y의 값이 반영
 
-// var partLi_0_H = partLi.eq(0).outerHeight() / 2;
-// var liOffset = partLi.eq(0).offset().top + partLi_0_H;
-// var partLi_0_img = partLi.eq(0).find('.img_box');
-
-
-
 // 함수
 var setScrollFn = function(liOffset){
   var scrollGage = win.scrollTop();
   // console.log( scrollGage - liOffset + winH);
   var moveCheck = scrollGage - liOffset + winH;
-
   var movePercent =  parseInt(moveCheck / winH * 100);
   if(movePercent < 0 ){ 
     movePercent = 0;
   }else if(movePercent > 100){
     movePercent = 100;
   }
-  console.log(movePercent);
   return movePercent;
 };
 
 var setLiFn = function(){
-
   // partLi의 각각의 순서에 맞는곳에 적용
-  // li의 갯수파악, 그 갯수만큼 반복
-  
+  // li의 갯수파악, 그 갯수만큼 반복  
   var st, liEq, liH, liLocation, liImg;
   var i = 0;
   var liLen = partLi.length;
   for(; i < liLen; i+=1){
     liEq       = partLi.eq(i);
-    liH        = liEq.outerHeight() / 2;
-    liLocation = liEq.offset().top + liH;
     liImg      = liEq.find('.img_box');
-    st         = setScrollFn(liH);
+    liH        = liImg.outerHeight() / 2; 
+    liLocation = liImg.offset().top + liH;
+    st         = 100 - setScrollFn(liLocation);
     liImg.css({backgroundPositionY:st+'%'});
   }
-
 }// setLiFn();
 
 
-
-// var st;
-// setScrollFn();
-
 // 이벤트
-win.on('scroll', function(e){
-  // st = 100 - setScrollFn();
-  // partLi_0_img.css({backgroundPositionY:st+'%'});
-  setLiFn();
-});
+setLiFn();
+win.on('scroll', function(){  setLiFn(); });
 
 
 })(jQuery);
