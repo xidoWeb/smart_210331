78e221652dc0cd20c5b08c47155f9c419dd5d521
xidoWeb <erivery2@gmail.com>
2021-07-28 12:02:30 +0900

    에러발생시 체크

--- code/js/src/jq_009_val.js
+++ code/js/src/jq_009_val.js
@@ -3,12 +3,32 @@
 // jQuery
 
 // 1. input(#inputResult)의 값을 확인
+// 2. 문제발생시 처리
+
 var inputR = $('#inputResult');
 var sendBtn = $('#send');
+var rowResult = $('.row_result');
+
+// 함수
+var innerP = function(data, text){
+  rowResult.html('<p></p>');
+  rowResult.find('p').addClass(data);
+  rowResult.find('p').text(text);
+}
+
+
+inputR.on('keyup', function(){
+  var inputValue = inputR.val();
+
+  if(inputValue.length < 2){
+    innerP('error', '두글자 이상 작성해주세요');
+  }else{
+    innerP('success', '두글자 이상 작성되었습니다.')
+  }
+});
 
 sendBtn.on('click', function(e){
   e.preventDefault();
-
   var inputValue = inputR.val();
   console.log( inputValue );
 });
