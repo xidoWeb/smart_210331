967d8613939c3fbcafaf9b2a048938a3536e403f
xidoWeb <erivery2@gmail.com>
2021-07-28 12:55:06 +0900

    내용 함수처리2

--- code/js/src/jq_009_val.js
+++ code/js/src/jq_009_val.js
@@ -13,35 +13,29 @@ var rowResult = $('.row_result');
 // 함수
 var innerP = function(data, text){
   rowResult.html('<p></p>');
-  rowResult.find('p').addClass(data);
-  rowResult.find('p').text(text);
-}
-
-
+  rowResult.find('p').eq(0).addClass(data);
+  rowResult.find('p').eq(0).text(text);
+};
+var innerP2 = function(data, text){
+  rowResult.append('<p></p>');
+  rowResult.find('p').eq(1).addClass(data);
+  rowResult.find('p').eq(1).text(text);
+};
 inputR.on('keyup', function(){
   var inputValue = inputR.val();
   var typeNumCheck = $.isNumeric(inputValue);
-
   if(inputValue.length < 2){
     innerP('error', '두글자 이상 작성해주세요');
   }else{
     innerP('success', '두글자 이상 작성되었습니다.');
-
     if(!typeNumCheck){
-      rowResult.append('<p></p>');
-      rowResult.find('p').eq(1).addClass('success');
-      rowResult.find('p').eq(1).text('감사합니다.');
+      innerP2('success', '감사합니다.');
     }
   }
-
   if(typeNumCheck){
-    rowResult.append('<p></p>');
-    rowResult.find('p').eq(1).addClass('error');
-    rowResult.find('p').eq(1).text('작성된 내용이 숫자입니다. 내용을 확인하고 다시 작성해주세요.');
+   var inText = '작성된 내용이 숫자입니다. 내용을 확인하고 다시 작성해주세요.';
+    innerP2('error', inText);
   }
-
-   // $.each([배열], function(i,d){})
-   // $.ajax({url:주소, context:document.body}).done(function(){})
 });
 
 sendBtn.on('click', function(e){
