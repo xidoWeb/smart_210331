d788837417cef87e8f8470cffd55f8a348ccaffc
xidoWeb <erivery2@gmail.com>
2021-08-10 13:25:41 +0900

    package기반의 nodesass명령 삽입

--- project/e_clone_web_01/index.js
+++ project/e_clone_web_01/index.js
@@ -1,20 +1,22 @@
 // 웹세팅
-
 import express from 'express'
 import path from 'path'
 import http from 'http'
 import logged from 'morgan'
-import sass from 'node-sass'
+// import sass from 'node-sass'
 
 // 기본설정-------------------------------
 const app = express()
 const port = 3000
+const __dirname = path.resolve()
+const staticDir = path.resolve(__dirname + '/public')
+app.use( express.static(staticDir) )
 app.use( logged('dev') )
 
 // 페이지구현 ---------------------------
-app.get('/', (request, response) => {
-  response.send('web !!!!!')
-})
+app.get('/', (request, response) => { response.sendFile(staticDir + '/html/main.html') })
+app.get('/main', (request, response) => {  response.sendFile(staticDir + '/html/main.html') })
+app.use((request, response)=>{ response.status(404).sendFile(staticDir + '/html/404.html') })
 
 // 서버구동 -----------------------------
 http.createServer(app).listen(port)
