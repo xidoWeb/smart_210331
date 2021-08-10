fc3d291402495ff3962331713a722206f6a0b3bc
xidoWeb <erivery2@gmail.com>
2021-08-10 15:51:26 +0900

    main html세팅

--- project/e_clone_web_01/index.js
+++ project/e_clone_web_01/index.js
@@ -3,15 +3,21 @@ import express from 'express'
 import path from 'path'
 import http from 'http'
 import logged from 'morgan'
-// import sass from 'node-sass'
+import sass from 'node-sass'
 
 // 기본설정-------------------------------
 const app = express()
-const port = 3000
+const port = 3000 
 const __dirname = path.resolve()
 const staticDir = path.resolve(__dirname + '/public')
 app.use( express.static(staticDir) )
 app.use( logged('dev') )
+
+// sass ---------------------
+const srcPath = path.resolve(staticDir + '/scss');
+const destPath = path.resolve(staticDir + '/css');
+
+
 
 // 페이지구현 ---------------------------
 app.get('/', (request, response) => { response.sendFile(staticDir + '/html/main.html') })
